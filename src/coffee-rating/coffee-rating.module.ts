0658336bd9b7a83e95adcba03e6617ad7f61bce8
Michael Jay <michael@merged.group>
2022-06-09 16:14:53 -0400

    Added in configuration

--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CoffeesModule } from 'src/coffees/coffees.module';
 import { DatabaseModule } from 'src/database/database.module';
 import { CoffeeRatingService } from './coffee-rating.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     DatabaseModule.register({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      password: 'pass123',
-      username: 'postgres'
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      password: process.env.DATABASE_PASSWORD,
+      username: process.env.DATABASE_USER
     }),
     CoffeesModule
   ],
