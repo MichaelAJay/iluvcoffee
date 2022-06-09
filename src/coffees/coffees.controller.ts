65b2ba732705de11d5ce8439485ae3551cac7fe2
Michael Jay <michael@merged.group>
2022-06-08 23:49:04 -0400

    Finished "Adding Pagination"

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -9,8 +9,7 @@ export class CoffeesController {
 
   @Get()
   findAll(@Query() paginationQuery) {
-    // const { limit, offset } = paginationQuery;
-    return this.coffeesService.findAll();
+    return this.coffeesService.findAll(paginationQuery);
   }
 
   @Get(':id')
