22db12c8f56c9be62ce677da274d65f8979b7528
Michael Jay <michael@merged.group>
2022-06-09 14:59:04 -0400

    Finished "Factory Providers"
    something was wrong with this one - it was throwing errors when I used the useFactory code

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Event } from 'src/events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
@@ -7,21 +7,11 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity'
 
-class ConfigService {}
-class DevelopmentConfigService {}
-class ProductionConfigService {}
-
 @Module({
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeesController],
     providers: [
         CoffeesService,
-        {
-            provide: ConfigService,
-            useClass: process.env.NODE_ENV === 'development'
-                ? DevelopmentConfigService
-                : ProductionConfigService
-        },
         { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] }
     ],
     exports: [CoffeesService]
